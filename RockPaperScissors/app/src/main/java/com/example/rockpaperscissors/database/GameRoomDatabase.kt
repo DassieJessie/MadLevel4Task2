7b7450a2621b94a759c8c083c3042e087c9d5030
Jienie12 <jr.egmond@gmail.com>
2020-11-05 17:57:57 +0100

    5.2 Create database

---
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/database/GameRoomDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.rockpaperscissors.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.rockpaperscissors.dao.GameDao
+import com.example.rockpaperscissors.model.Game
+
+@Database(entities = [Game::class], version = 1, exportSchema = false)
+abstract class GameRoomDatabase : RoomDatabase(){
+
+    abstract fun gameDao(): GameDao
+
+    companion object {
+        private const val DATABASE_NAME = "GAME_DATABASE"
+
+        @Volatile
+        private var reminderRoomDatabaseInstance: GameRoomDatabase? = null
+
+        fun getDatabase(context: Context): GameRoomDatabase? {
+
+            //make database
+            if (reminderRoomDatabaseInstance == null) {
+                synchronized(GameRoomDatabase::class.java) {
+                    if (reminderRoomDatabaseInstance == null) {
+                        reminderRoomDatabaseInstance = Room.databaseBuilder(
+                            context.applicationContext,
+                            GameRoomDatabase::class.java, DATABASE_NAME
+                        )
+                            .allowMainThreadQueries()
+                            .build()
+                    }
+                }
+            }
+
+            return reminderRoomDatabaseInstance
+        }
+    }
+}
